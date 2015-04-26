259b019531e0a50ec0be6b30430ffd518bb2e7a4
Matt Gross <mattkgross@gmail.com>
2015-04-25 19:08:58 -0600

    First pass at Product class

--- models/Product.php
+++ models/Product.php
@@ -1,15 +1,63 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: Matt
- * Date: 4/25/2015
- * Time: 6:42 PM
- */
-
 namespace app\models;
-
 
 class Product
 {
+    private $id; // int
+    private $name; // string
+    private $description; // string
+    private $website; // string
+    private $os; // string
+
+    function __construct()
+    {
+        // Create product id somehow? Lol.
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function getWebsite()
+    {
+        return $this->website;
+    }
+
+    public function getOs()
+    {
+        return $this->os;
+    }
+
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    public function setWebsite($website)
+    {
+        $this->website = $website;
+    }
+
+    public function setOs($os)
+    {
+        $this->os = $os;
+    }
 
 }
--- models/Review.php
+++ models/Review.php
@@ -1,13 +1,5 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: Matt
- * Date: 4/25/2015
- * Time: 6:42 PM
- */
-
 namespace app\models;
-
 
 class Review
 {
